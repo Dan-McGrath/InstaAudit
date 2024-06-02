949200b80cc8f717aa6589dc724d9e32ce39d90b
Dan McGrath <mcgrathd3254@yahoo.com>
2024-06-02 06:13:16 -0400

    Create basic router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,19 @@
-function App() {
+import { Route, Routes } from 'react-router';
+import Layout from './components/layout/Layout';
+import ErrorPage from './pages/ErrorPage';
+import Home from './pages/Home';
+import Login from './auth/Login';
+
+const App = () => {
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <Routes>
+      <Route path="/" element={<Layout />} errorElement={<ErrorPage />}>
+        <Route path="*" element={<ErrorPage />} />
+        <Route index element={<Home />} />
+        <Route path="login" element={<Login />} />
+      </Route>
+    </Routes>
   );
-}
+};
 
 export default App;
