921685d2c26fa059fc0be9575e3121d334f2d7c1
Dan McGrath <mcgrathd3254@yahoo.com>
2024-06-02 02:30:48 -0400

    Extend colors in tailwind config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,7 +3,13 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
-      colors: {},
+      colors: {
+        primaryColor: '#004D7F',
+        backgroundColor: '#F5F5F5',
+        secondaryColor: '#00B7B6',
+        accentColor: '#FFD400',
+        white: '#FFFFFF',
+      },
       fontFamily: {},
     },
   },
