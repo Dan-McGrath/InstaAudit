05ad038cdc974fdc725cf23b1214a8de4ae5fddf
Dan McGrath <mcgrathd3254@yahoo.com>
2024-06-03 17:34:26 -0400

    create controller for hotel data

---
+++ backend/controllers/hotelDataController.js
@@ -0,0 +1,139 @@
+const sampleData = require("../db/data");
+
+// @desc Get all hotel data
+// @route GET /hotelData
+// @access Private
+const getAllData = async (req, res) => {
+  // TODO: change when real DB is added
+  const data = sampleData;
+
+  if (!data?.length) {
+    return res.status(400).json({ message: "No hotel data found" });
+  }
+
+  res.json(users);
+};
+
+// @desc Get specific user
+// @route POST hotel data
+// @access Private
+const submitData = async (req, res) => {
+  const {
+    groupData,
+    compRoomInfo,
+    outOfOrderRooms,
+    revenueData,
+    roomData,
+    auditDate,
+    createdBy,
+    createdAt,
+    updatedAt,
+  } = req.body;
+  // Check if revenueData is correct
+  if (
+    !Number(revenueData.total) ||
+    !Number(revenueData.group) ||
+    !Number(revenueData.contract)
+  ) {
+    return res
+      .status(400)
+      .json({ message: "All revenue data required. Fields must be numbers" });
+  }
+
+  // Check if roomData is correct
+  if (
+    !Number(roomData.total) ||
+    !Number(roomData.contract) ||
+    !Number(roomData.outOfOrderRooms) ||
+    !Number(roomData.stayOver) ||
+    !Number(roomData.arrivals) ||
+    !Number(roomData.departures) ||
+    !Number(roomData.groupRooms) ||
+    !Number(roomData.compRooms)
+  ) {
+    return res.status(400).json({
+      message:
+        "All room data required even if value is 0. Fields must be numbers",
+    });
+  }
+
+  // if room data has group rooms but the group data is not inputted. Reject the request
+  if (!(groupData.length === roomData.groupRooms)) {
+    return res.status(400).json({
+      message: "Must add group data too submit",
+    });
+  }
+
+  // if room data has comp rooms but the compData is not inputted. Reject the request
+  if (!(compRoomInfo.length === roomData.compRooms)) {
+    return res.status(400).json({
+      message: "Must add complementary room data too submit",
+    });
+  }
+
+  // Check comp room info if any entered
+  if (compRoomInfo) {
+    compRoomInfo.forEach((ele) => {
+      if (!ele.authorizedBy || !ele.guestName || !ele.reason) {
+        return res.status(400).json({
+          message: "All fields required for comp room info",
+        });
+      }
+    });
+  }
+
+  // Check out of order info if any entered
+  if (outOfOrderRooms) {
+    outOfOrderRooms.forEach((ele) => {
+      if (!ele.code || !ele.roomNumber || !ele.details) {
+        return res.status(400).json({
+          message: "All fields required for out of order room info",
+        });
+      }
+    });
+  }
+
+  // Check group info if any entered
+  if (groupData) {
+    groupData.forEach((ele) => {
+      if (!ele.name || !Number(ele.roomCount) || !Number(ele.revenue)) {
+        return res.status(400).json({
+          message: "All fields required for group data",
+        });
+      }
+    });
+  }
+
+  // Check if dates are date objects
+  if (
+    !(auditDate instanceof Date) ||
+    !(createdBy instanceof Date) ||
+    !(createdAt instanceof Date) ||
+    !(updatedAt instanceof Date)
+  ) {
+    return res.status(400).json({
+      message: "Dates are not a date object",
+    });
+  }
+
+  // TODO: Change to add to DB when added
+  const auditData = {
+    groupData,
+    compRoomInfo,
+    outOfOrderRooms,
+    revenueData,
+    auditDate,
+    createdBy,
+    createdAt,
+    updatedAt,
+  };
+
+  sampleData.push(auditData);
+
+  res.status(201).json({ message: `Info for ${auditDate} created` });
+};
+
+module.exports = {
+  getAllData,
+  submitData,
+};
