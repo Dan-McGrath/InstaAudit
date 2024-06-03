e3ab934ef40f573d687c1f573a6a277dbab61ae3
Dan McGrath <mcgrathd3254@yahoo.com>
2024-06-03 17:37:52 -0400

    Update useHotelData hook to use fetch requests

--- frontend/src/store/useHotelData.jsx
+++ frontend/src/store/useHotelData.jsx
@@ -1,44 +1,81 @@
 import { createContext, useContext, useMemo, useState, useEffect } from 'react';
-import { data } from '../db/data';
-import { hotelDetails } from '../db/hotelDetails';
-const HotelContext = createContext({});
 
-const sampleData = data;
-const sampleHotelDetails = hotelDetails;
+const HotelContext = createContext({});
 
 export const HotelProvider = ({ children }) => {
   // TODO: Make fetch requests to get hotel data
-  const [data, setData] = useState({});
-  const [hotelDetails, setHotelDetails] = useState([]);
+  const [auditData, setAuditData] = useState({});
+  const [hotelDetails, setHotelDetails] = useState({});
   const [hotelData, setHotelData] = useState([]);
   const [auditDate, setAuditDate] = useState(Date.now());
+  const [hotelOverviewData, setHotelOverviewData] = useState({});
+
+  // URL
+  const fetchURL = 'http://localhost:3500';
+  const hotelDetailEndpoint = '/hotelDetail';
+  const hotelDataEndpoint = '/hotelData';
+
+  const getHotelOverviewData = async () => {
+    const response = await fetch(fetchURL + hotelDataEndpoint + '/overview', {
+      method: 'GET',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    setHotelOverviewData(response.json());
+  };
 
   const submitData = async (data) => {
-    setHotelData([...hotelData, data]);
-    setData(data);
+    const response = await fetch(fetchURL + hotelDataEndpoint, {
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
+    setHotelData([...hotelData, response.json()]);
+    setAuditData(response.json());
   };
 
   const fetchHotelData = async () => {
-    setHotelData(sampleData);
+    const response = await fetch(fetchURL + hotelDataEndpoint, {
+      method: 'GET',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    setHotelData(response.json);
   };
 
   useEffect(() => {
     const fetchHotelDetails = async () => {
-      setHotelDetails(sampleHotelDetails);
+      const response = await fetch(fetchURL + hotelDetailEndpoint, {
+        method: 'GET',
+        mode: 'cors',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      setHotelDetails(response.json());
     };
     fetchHotelDetails();
   }, []);
 
   const value = useMemo(
     () => ({
-      data,
+      auditData,
       hotelDetails,
       hotelData,
       auditDate,
+      hotelOverviewData,
       fetchHotelData,
       submitData,
+      setAuditDate,
     }),
-    [data, hotelDetails, hotelData, auditDate],
+    [auditData, hotelDetails, hotelData, auditDate, hotelOverviewData],
   );
 
   return <HotelContext.Provider value={value}>{children}</HotelContext.Provider>;
