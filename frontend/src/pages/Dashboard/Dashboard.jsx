73d626894fdccb80522b26b522cbe98eb2227c28
Dan McGrath <mcgrathd3254@yahoo.com>
2024-06-06 01:10:25 -0400

    Add audit date as well as Occupancy and revenue data

--- frontend/src/pages/Dashboard/Dashboard.jsx
+++ frontend/src/pages/Dashboard/Dashboard.jsx
@@ -1,9 +1,35 @@
+import Card from '../../components/Card';
 import MultiChartContainer from '../../components/charts/MultiChartContainer';
+import { useHotelData } from '../../store/useHotelData';
 import OverviewTiles from './OverviewTiles';
+import { format } from 'date-fns';
+import { formatToDollars } from '../../utils/helper';
 
 const Dashboard = () => {
+  const { auditDate, setAuditDate, hotelOverviewData } = useHotelData();
+  const occupancy = hotelOverviewData.occupancy;
+  console.log(occupancy);
   return (
-    <main>
+    <main className="mt-5">
+      <div className="pr-3 mb-3 text-right">
+        <p>
+          Audit Date: <span className="text-primaryColor">{format(auditDate, 'MMMM dd, yyyy')}</span>
+        </p>
+      </div>
+      {Object.keys(hotelOverviewData).length !== 0 && (
+        <div className="flex flex-col gap-2 px-5 py-3 mx-2 border rounded-md shadow-lg justify-evenly">
+          <Card
+            data={
+              occupancy.toLocaleString(undefined, {
+                minimumFractionDigits: 2,
+                maximumFractionDigits: 2,
+              }) + '%'
+            }
+            text="Occupancy"
+          />
+          <Card data={formatToDollars(hotelOverviewData.revenueData.total)} text="Room Revenue" />
+        </div>
+      )}
       <div>
         <OverviewTiles />
       </div>
