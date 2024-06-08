7a7550851aeceefae9244b35780b8717aab38cba
Dan McGrath <mcgrathd3254@yahoo.com>
2024-06-08 18:13:20 -0400

    Add states for input info

--- frontend/src/pages/Audit/AuditForm.jsx
+++ frontend/src/pages/Audit/AuditForm.jsx
@@ -1,5 +1,32 @@
+import { useState } from 'react';
+import { useHotelData } from '../../store/useHotelData';
+
 const AuditForm = () => {
-  return <div>AuditForm</div>;
+  const { sumbitData, handleChange } = useHotelData();
+
+  // Revenue data
+  const [roomRevenue, setRoomRevenue] = useState(0);
+  const [groupRevenue, setGroupRevenue] = useState(0);
+  const [contractRevenue, setcontractRevenue] = useState(0);
+
+  // Room Data
+  const [totalRooms, setTotalRooms] = useState(0);
+  const [arrivals, setArrivals] = useState(0);
+  const [contractRooms, setContractRooms] = useState(0);
+  const [departures, setDepartures] = useState(0);
+  const [outOfOrderRoomTotal, setOutOfOrderRoomTotal] = useState(0);
+  const [stayOvers, setStayOvers] = useState(0);
+
+  // Arrays of information for subsections
+  const [groupData, setGroupData] = useState([]);
+  const [compRoomInfo, setCompRoomInfo] = useState([]);
+  const [outOfOrderRooms, setOutOfOrderRooms] = useState([]);
+
+  return (
+    <form onSubmit={(e) => sumbitData(e)}>
+      <input type="submit" disabled />
+    </form>
+  );
 };
 
 export default AuditForm;
