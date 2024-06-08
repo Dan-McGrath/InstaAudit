eec2a4d4066c8a21e70789a44c0c9cbc2fc29b57
Dan McGrath <mcgrathd3254@yahoo.com>
2024-06-08 17:41:16 -0400

    Setup routing for audit page and audit/submit route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,9 @@ import Home from './pages/Home/Home';
 import Login from './auth/Login';
 import Dashboard from './pages/Dashboard/Dashboard';
 import { HotelProvider } from './store/useHotelData';
+import Audit from './pages/Audit/Audit';
+import AuditForm from './pages/Audit/AuditForm';
+import AuditOverview from './pages/Audit/AuditOverview';
 
 const App = () => {
   return (
@@ -16,8 +19,13 @@ const App = () => {
           <Route path="login" element={<Login />} />
         </Route>
         <Route path="dashboard" element={<Layout />} errorElement={<ErrorPage />}>
-          <Route path="*" element={<ErrorPage />} />
           <Route index element={<Dashboard />} />
+          <Route path="audit" element={<Audit />}>
+            <Route index element={<AuditOverview />} />
+            <Route path="submit" element={<AuditForm />} />
+            <Route path="*" element={<ErrorPage />} />
+          </Route>
+          <Route path="*" element={<ErrorPage />} />
         </Route>
       </Routes>
     </HotelProvider>
