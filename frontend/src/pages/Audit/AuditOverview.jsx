b33e716fe8716ddffa72585b2b0ae35e4675e2b8
Dan-McGrath <mcgrathd3254@yahoo.com>
2024-06-12 17:02:54 -0400

    Check is audit data hs been submitted and show correct component

--- frontend/src/pages/Audit/AuditOverview.jsx
+++ frontend/src/pages/Audit/AuditOverview.jsx
@@ -1,5 +1,28 @@
+import { useHotelData } from '../../store/useHotelData';
+import { useNavigate } from 'react-router-dom';
+
 const AuditOverview = () => {
-  return <div>AuditOverview</div>;
+  const { auditData } = useHotelData();
+  const navigate = useNavigate();
+  return Object.keys(auditData).length !== 0 ? (
+    <>
+      <h2>Audit Data</h2>
+    </>
+  ) : (
+    <>
+      <h2 className="my-6 text-3xl font-bold text-center text-primaryColor">Audit Data</h2>
+      <div className="flex flex-col items-center w-3/4 gap-2 px-2 py-3 mx-auto text-center border rounded-md shadow-lg inset-5 text-pretty">
+        <p>No audit data submited.</p>
+        <p>Please submit audit to see data.</p>
+        <button
+          className="px-3 py-2 my-3 text-white rounded-full font-cta bg-primaryColor hover:bg-primaryColor/85"
+          onClick={() => navigate('./submit', { relative: 'path' })}
+        >
+          Submit Audit
+        </button>
+      </div>
+    </>
+  );
 };
 
 export default AuditOverview;
