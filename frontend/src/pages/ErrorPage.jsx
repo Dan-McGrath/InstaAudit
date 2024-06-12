527bb53e4c924e47cad3e13d899573e433dd4305
Dan-McGrath <mcgrathd3254@yahoo.com>
2024-06-12 17:02:27 -0400

    update error page styles and text

--- frontend/src/pages/ErrorPage.jsx
+++ frontend/src/pages/ErrorPage.jsx
@@ -1,5 +1,14 @@
+import { useRouteError } from 'react-router-dom';
+
 const ErrorPage = () => {
-  return <div>ErrorPage</div>;
+  return (
+    <main className="mt-12">
+      <div className="flex flex-col w-3/4 gap-4 mx-auto text-center text-pretty">
+        <h2 className="text-3xl font-bold text-primaryColor">404 Error</h2>
+        <p>Uh oh looks like there was an error...</p>
+      </div>
+    </main>
+  );
 };
 
 export default ErrorPage;
