fde46b2a82df1e615a05e2572bf206549b36aa08
Dan McGrath <mcgrathd3254@yahoo.com>
2024-06-15 18:59:53 -0400

    update props and add control to input

--- frontend/src/pages/Audit/AuditForm/RevenueDataForm.jsx
+++ frontend/src/pages/Audit/AuditForm/RevenueDataForm.jsx
@@ -1,16 +1,22 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
-const RevenueDataForm = () => {
+const RevenueDataForm = ({ setAduitData, auditData, error, setError }) => {
   // Revenue data
-  const [roomRevenue, setRoomRevenue] = useState();
+  const [roomRevenue, setRoomRevenue] = useState(0);
   const [groupRevenue, setGroupRevenue] = useState();
   const [contractRevenue, setcontractRevenue] = useState();
 
   return (
     <>
       <div className="flex justify-between w-full gap-2 mx-auto">
-        <label htmlFor="roomRevenue">Room Revenue</label>
-        <input type="number" id="roomRevenue" className="w-1/2 rounded-md focus:border-primaryColor" />
+        <label htmlFor="roomRevenue">Total Room Revenue</label>
+        <input
+          type="number"
+          id="roomRevenue"
+          className="w-1/2 rounded-md focus:border-primaryColor text-primaryColor"
+          onChange={(e) => setRoomRevenue(e.target.value)}
+          value={roomRevenue}
+        />
       </div>
       <div className="flex justify-between w-full gap-2 mx-auto">
         <label htmlFor="groupRevenue">Group Revenue</label>
