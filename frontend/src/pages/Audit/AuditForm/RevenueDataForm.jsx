7b0e35e78d6739831fcf53b71932941dfef87b8e
Dan McGrath <mcgrathd3254@yahoo.com>
2024-06-15 06:23:43 -0400

    Add inputs and labels as well as add styling to elements

--- frontend/src/pages/Audit/AuditForm/RevenueDataForm.jsx
+++ frontend/src/pages/Audit/AuditForm/RevenueDataForm.jsx
@@ -8,8 +8,18 @@ const RevenueDataForm = () => {
 
   return (
     <>
-      <label htmlFor="roomRevenue">Room Revenue</label>
-      <input type="number" id="roomRevenue" />
+      <div className="flex justify-between w-full gap-2 mx-auto">
+        <label htmlFor="roomRevenue">Room Revenue</label>
+        <input type="number" id="roomRevenue" className="w-1/2 rounded-md focus:border-primaryColor" />
+      </div>
+      <div className="flex justify-between w-full gap-2 mx-auto">
+        <label htmlFor="groupRevenue">Group Revenue</label>
+        <input type="number" id="groupRevenue" className="w-1/2 rounded-md" />
+      </div>
+      <div className="flex justify-between w-full gap-2 mx-auto">
+        <label htmlFor="contractRevenue">Contract Revenue</label>
+        <input type="number" id="contractRevenue" className="w-1/2 rounded-md" />
+      </div>
     </>
   );
 };
